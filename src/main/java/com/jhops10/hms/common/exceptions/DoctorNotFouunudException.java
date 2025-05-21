e3f1a0fd6fdd627e7081b595fcc975b3460a764a
João Paulo <joaowais@gmail.com>
2025-05-20 23:18:15 -0300

    add(DoctorNotFoundException): adiciona exception

---
+++ src/main/java/com/jhops10/hms/common/exceptions/DoctorNotFouunudException.java
@@ -0,0 +1,8 @@
+package com.jhops10.hms.common.exceptions;
+
+public class DoctorNotFouunudException extends RuntimeException {
+
+    public DoctorNotFouunudException(String message) {
+        super(message);
+    }
+}
